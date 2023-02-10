23982ad148357e90933fdf9993d0ebcbd965bf6c
Alex Muench <ammuench@gmail.com>
2023-02-09 23:09:28 -0600

    ➕ Add testing deps, setup route module structure
    * Add supertest for E2E tests
    * Setup routes folder, connect express from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
+// Router Imports
+
+import IndexRoutes from "./routes/index";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
+app.use("/", IndexRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
