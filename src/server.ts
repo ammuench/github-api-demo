b3417a33446875be0c4170cb1f45122f67d3524f
Alex Muench <ammuench@gmail.com>
2023-02-10 00:08:20 -0600

    🔧 Setup config for loading github api token

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 // Router Imports
-
 import IndexRoutes from "./routes/index";
+
+// Check for token
+dotenv.config();
+if (!process.env.GITHUB_ACCESS_TOKEN) {
+  throw new Error("No Github Access Token Provided in process.env file");
+}
+
 
 const app = express();
 const port = 3000;
