8cf520ab33141c9d9b3a59871baa2e5d29fa281b
Alex Muench <ammuench@gmail.com>
2023-02-11 13:34:07 -0600

    🏷️ Build general types for Github API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,7 @@ import githubApi from "../api/apiBase";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  // res.json("Index Route Get");
-  const githubRes = await githubApi.get("/repos/ammuench/node-dota-api");
+  const githubRes = await githubApi.get("/repos/jezen/is-thirteen/pulls/748/commits");
   const { data } = githubRes;
   res.json(data);
 });
